e294a9171b49c23f264fe585b1282bf3857daeb0
Lance Edgar <lance@edbob.org>
2024-01-10 10:54:21 -0600

    Do not lazy-load the about view
    apparently that isn't working for published q-app, and maybe offers no
    benefit anyway since the whole app is local..? can investigate more
    later if needed

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 import QortalRequestView from '../views/QortalRequestView.vue'
 
 const router = createRouter({
@@ -23,10 +24,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView,
     }
   ]
 })
