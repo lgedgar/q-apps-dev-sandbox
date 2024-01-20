cf0a61307bee40b82990cb3640f12288302a8c88
Lance Edgar <lance@edbob.org>
2024-01-19 21:07:59 -0600

    Show misc. info on About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,61 @@
+<script setup>
+import appsettings from '../appsettings'
+</script>
+
+<script>
+export default {
+
+    data() {
+        return {
+            appTitle: appsettings.appTitle,
+            appVersion: appsettings.appVersion,
+            appDependencies: appsettings.appDependencies,
+            appRepository: appsettings.appRepository,
+            appMaintainer: appsettings.maintainerName,
+        }
+    },
+
+    methods: {
+
+        async showMaintainer() {
+            await qortalRequest({
+                action: 'OPEN_PROFILE',
+                name: this.appMaintainer,
+            })
+        },
+    },
+}
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+
+    <o-field label="App Name" horizontal>
+      <span>{{ appTitle }}</span>
+    </o-field>
+
+    <o-field label="App Version" horizontal>
+      <span>{{ appVersion }}</span>
+    </o-field>
+
+    <o-field label="Dependencies" horizontal>
+      <ul>
+        <li v-for="(ver, pkg, i) in appDependencies"
+            :key="pkg">
+          {{ pkg }} {{ ver }}
+        </li>
+      </ul>
+    </o-field>
+
+    <o-field label="Maintainer" horizontal>
+      <a href="#" @click.prevent="showMaintainer()">
+        {{ appMaintainer }}
+      </a>
+    </o-field>
+
+    <o-field label="Code Repository" horizontal>
+      <span>{{ appRepository }}</span>
+    </o-field>
+
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
